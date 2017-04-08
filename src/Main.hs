72cd762a92e152c2cd205e09ab5493b189610ea1
David Obwaller <david.obwaller@student.uibk.ac.at>
2017-04-08 12:23:53 +0200

    Basic framework for chat bots

--- src/Main.hs
+++ src/Main.hs
@@ -2,9 +2,8 @@
 
 module Main where
 
-import           Control.Concurrent (forkIO)
 import           Control.Lens       ((&), (.~), (^.))
-import           Control.Monad      (forever, unless, void)
+import           Control.Monad      (forever, void)
 import           Data.Aeson
 import           Data.Text          (Text)
 import qualified Data.Text          as T
@@ -111,55 +110,101 @@ main = do
     _ -> putStrLn "error"
 
 bot :: Auth -> String -> String -> IO ()
-bot auth host path = runSecureClient host 443 path (ws auth)
+bot auth host path = runSecureClient host 443 path (makeClientApp echo)
 
-ws :: Auth -> ClientApp ()
-ws auth conn = do
+newtype Slackbot c
+  = Slackbot {
+    slackbot :: MessageContext -> c -> [MessagePart] -> ([Outgoing], c)
+  }
+
+data MessageContext =
+  MessageContext {
+    mcChannel :: Text
+  }
+  deriving (Eq, Show)
+
+makeClientApp :: Slackbot () -> ClientApp ()
+makeClientApp bot conn = do
   putStrLn "Connected!"
 
-  forever $ do
+  void . forever $ do
     message <- receiveData conn
     case decode message :: Maybe Incoming of
       Just im@IncomingMessage{..} -> do
         print im
-        case parse p mempty imText of
-          Right ast -> do
-            let om = OutgoingMessage 1 "message" imChannel (T.pack . show $ ast)
-            sendTextData conn (encode om)
-          Left e ->
-            putStrLn ">>> NO PARSE"
+        case parse userMessage mempty imText of
+          Right msg -> do
+            let mc = MessageContext imChannel
+            let (out, _) = slackbot bot mc () msg
+            mapM_ (sendTextData conn . encode) out
+          Left _ ->
+            putStrLn ">>> could not parse message text"
       _ ->
         putStrLn ">>> unknown incoming message format"
 
   sendClose conn ("Bye!" :: Text)
 
-data Term
-  = F Text [Term]
-  | V Text
-  deriving (Show)
+-- * Message parsing
 
-term :: Parser Term
-term = try fun <|> var
+data MessagePart
+  = Message {
+    msgText :: Text
+  }
+  | UserRef {
+    urId :: Text
+  }
+  | ChannelRef {
+    crId   :: Text
+  , crName :: Text
+  }
+  | UrlRef {
+    crUrl :: Text
+  }
+  deriving (Eq, Show)
+
+userMessage :: Parser [MessagePart]
+userMessage =
+    many1 $ msg <|> ref
   where
-    ident :: Parser Text
-    ident = T.pack <$> many1 letter
+    msg = Message . T.pack <$> many1 (noneOf "<")
 
-    fun :: Parser Term
-    fun = do
-      f <- ident
-      char '('
-      ts <- term `sepBy` char ','
-      char ')'
-      return $ F f ts
+ref :: Parser MessagePart
+ref = try userRef <|> try channelRef <|> urlRef
 
-    var :: Parser Term
-    var = V <$> ident
-
-p :: Parser Term
-p = do
+-- Example: <@U4WLZM16J>
+userRef :: Parser MessagePart
+userRef = do
   char '<'
   char '@'
-  many1 alphaNum
+  i <- T.pack <$> many1 alphaNum
   char '>'
-  many1 space
-  term
+  return $ UserRef $ i
+
+-- Example: <#C4WLY8BT8|random>
+channelRef :: Parser MessagePart
+channelRef = do
+  char '<'
+  char '#'
+  i <- T.pack <$> many1 alphaNum
+  char '|'
+  n <- T.pack <$> many1 alphaNum
+  char '>'
+  return $ ChannelRef i n
+
+-- Example: <http://www.google.at>
+urlRef :: Parser MessagePart
+urlRef = do
+  char '<'
+  url <- T.pack <$> many1 (noneOf ">")
+  char '>'
+  return $ UrlRef url
+
+-- * Echo bot
+
+echo :: Slackbot ()
+echo = Slackbot f
+  where
+    f :: MessageContext -> () -> [MessagePart] -> ([Outgoing], ())
+    f MessageContext{..} context message = ([om], context)
+      where
+        om = OutgoingMessage 1 "message" mcChannel (T.pack . show $ message)
